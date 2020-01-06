4790bcd9ede6eb4b3ea1dcd7b571860810a941ed
Abhishek Bhandari <11881672+abhishekb91@users.noreply.github.com>
2020-01-05 19:10:24 -0500

    Added Bootstrap to the application
    [*] Updated templates file to include bootstrap tags
    
    [*] Updated requirements.txt to include flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
